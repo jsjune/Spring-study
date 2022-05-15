c6b9325fd82da1e5ca83199e0a0476b99ee228b4
jsjune <avraskio@gmail.com>
2022-05-15 23:27:35 +0900

    querydsl 설정

---
+++ querydslPrac/src/main/java/com/querydslprac/QuerydslPracApplication.java
@@ -0,0 +1,13 @@
+package com.querydslprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslPracApplication.class, args);
+    }
+
+}
