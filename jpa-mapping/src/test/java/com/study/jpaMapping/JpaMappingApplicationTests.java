8af4a2efc93fa277e411b2c2abd00947d5e8b6b0
jsjune <avraskio@gmail.com>
2022-05-15 23:26:57 +0900

    test

---
+++ jpa-mapping/src/test/java/com/study/jpaMapping/JpaMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpaMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
