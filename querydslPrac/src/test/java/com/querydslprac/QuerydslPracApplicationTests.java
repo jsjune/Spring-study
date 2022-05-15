0a0e7d3a4281000931a678bcd389aecaaa28d45d
jsjune <avraskio@gmail.com>
2022-05-16 00:17:34 +0900

    application.yml properties 설정

--- querydslPrac/src/test/java/com/querydslprac/QuerydslPracApplicationTests.java
+++ querydslPrac/src/test/java/com/querydslprac/QuerydslPracApplicationTests.java
@@ -1,13 +1,39 @@
 package com.querydslprac;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydslprac.entity.Hello;
+import com.querydslprac.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
+@Transactional
 class QuerydslPracApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QHello qHello = new QHello("h");
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
