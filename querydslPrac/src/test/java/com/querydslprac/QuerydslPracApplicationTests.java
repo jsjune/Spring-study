6d749b954d884a49b6f9600a941ea260f2245316
jsjune <avraskio@gmail.com>
2022-05-16 05:34:33 +0900

    practice

--- querydslPrac/src/test/java/com/querydslprac/QuerydslPracApplicationTests.java
+++ querydslPrac/src/test/java/com/querydslprac/QuerydslPracApplicationTests.java
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
