9a5fc45121eefddecf966be68aa3e5ffc73d7a30
jsjune <avraskio@gmail.com>
2022-08-09 17:12:53 +0900

    update

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -18,7 +18,12 @@ import org.springframework.context.annotation.Import;
 //@Import(BeanPostProcessorConfig.class)
 //@Import(AutoProxyConfig.class)
 @Import(AopConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
+/*
+컴포넌트 스캔을 시작할 위치를 지정한다 주의
+컴포넌트 스캔에 의해 hello.proxy.config 위치의 설정 파일들이 스프링 빈으로 자동 등록 되지
+않도록 컴포넌스 스캔의 시작 위치를 scanBasePackages=hello.proxy.app 로 설정해야 한다.
+*/
 public class ProxyApplication {
 
 	public static void main(String[] args) {
