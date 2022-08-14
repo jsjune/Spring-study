076186878d9eac64e4cccb543e99eca5de2d2b73
jsjune <avraskio@gmail.com>
2022-08-14 23:31:18 +0900

    update

--- core2/src/main/java/study/core2/Core2Application.java
+++ core2/src/main/java/study/core2/Core2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Core2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Core2Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Core2Application.class, args);
+	}
 
 }
