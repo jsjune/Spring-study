285564a90a6c8d2f2d399c410db9ae1bffc17989
jsjune <avraskio@gmail.com>
2022-08-25 02:47:02 +0900

    update

---
+++ springtx/src/main/java/study/springtx/order/NotEnoughMoneyException.java
@@ -0,0 +1,7 @@
+package study.springtx.order;
+
+public class NotEnoughMoneyException extends Exception{
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
+}
---
+++ springtx/src/main/java/study/springtx/order/Order.java
@@ -0,0 +1,22 @@
+package study.springtx.order;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+@Setter
+@Getter
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username; //정상, 예외, 잔고부족
+    private String payStatus; //대기, 완료
+}
