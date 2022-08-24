3deb3f74b3bca7d861bff7a99b9ca6fc177622ef
jsjune <avraskio@gmail.com>
2022-08-25 00:00:58 +0900

    update

---
+++ springtx/src/test/java/study/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
