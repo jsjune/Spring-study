b11a31a1aa8fa7b0085a61e4cd352b2ea0db60f1
jsjune <avraskio@gmail.com>
2022-09-05 18:42:06 +0900

    update

---
+++ dockerstart/src/main/java/study/dockerstart/DockerstartApplication.java
@@ -0,0 +1,13 @@
+package study.dockerstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerstartApplication.class, args);
+    }
+
+}
---
+++ dockerstart/src/test/java/study/dockerstart/DockerstartApplicationTests.java
@@ -0,0 +1,13 @@
+package study.dockerstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
