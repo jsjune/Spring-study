9b4bc508fc2241736fa86dadde8df15f822736c0
jsjune <avraskio@gmail.com>
2022-09-26 11:17:58 +0900

    update

---
+++ junit5Prac/src/main/java/study/junit5prac/Junit5PracApplication.java
@@ -0,0 +1,13 @@
+package study.junit5prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5PracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5PracApplication.class, args);
+    }
+
+}
---
+++ junit5Prac/src/test/java/study/junit5prac/Junit5PracApplicationTests.java
@@ -0,0 +1,13 @@
+package study.junit5prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5PracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
