09edd64834400f69fb38c3c2fcc016cf40135b3a
jsjune <avraskio@gmail.com>
2022-10-06 01:46:35 +0900

    :memo: update

---
+++ algorithm/src/sortingAndSearching/solution/BubbleSort.java
@@ -0,0 +1,30 @@
+package sortingAndSearching.solution;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        BubbleSort t = new BubbleSort();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int num : t.solution(n, arr)) {
+            System.out.print(num + " ");
+        }
+    }
+}
