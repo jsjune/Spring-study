5fe874d77d1ee6fe992fc74a7579958087fddaf0
jsjune <avraskio@gmail.com>
2022-10-05 10:21:05 +0900

    update

---
+++ algorithm/src/sortingAndSearching/solution/SelectionSort.java
@@ -0,0 +1,33 @@
+package sortingAndSearching.solution;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            int idx = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    idx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort t = new SelectionSort();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int num : t.solution(n, arr)) {
+            System.out.print(num + " ");
+        }
+    }
+}
