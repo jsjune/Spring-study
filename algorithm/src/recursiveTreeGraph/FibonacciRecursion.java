9edf4d8fa32a9fba7df66c01a5d337c5d02b4022
jsjune <avraskio@gmail.com>
2022-10-20 12:42:31 +0900

    :memo: update

--- algorithm/src/recursiveTreeGraph/FibonacciRecursion.java
+++ algorithm/src/recursiveTreeGraph/FibonacciRecursion.java
@@ -9,14 +9,18 @@ package recursiveTreeGraph;
  * */
 
 public class FibonacciRecursion {
+    static  int[] fibo;
     public int DFS(int n) {
-        if(n==1) return 1;
-        else if (n == 2) return 1;
-        else return DFS(n-2)+DFS(n-1);
+        if(fibo[n]>0) return fibo[n];
+        if(n==1) return fibo[n]=1;
+        else if (n == 2) return fibo[n]=1;
+        else return fibo[n]=DFS(n-2)+DFS(n-1);
     }
     public static void main(String[] args) {
         FibonacciRecursion t = new FibonacciRecursion();
-        int n=10;
-        for(int i=1;i<=n;i++) System.out.print(t.DFS(i)+" ");
+        int n=45;
+        fibo = new int[n + 1];
+        t.DFS(n);
+        for(int i=1;i<=n;i++) System.out.print(fibo[i]+" ");
     }
 }
