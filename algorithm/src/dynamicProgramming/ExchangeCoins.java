aa76a600ab40f67a58a73f9d81ed77ecd3edcabf
jsjune <avraskio@gmail.com>
2022-12-19 14:05:08 +0900

    :memo: update

---
+++ algorithm/src/dynamicProgramming/ExchangeCoins.java
@@ -0,0 +1,34 @@
+package dynamicProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ExchangeCoins {
+    static int n, m;
+    static int[] dy;
+
+    public int solution(int[] coin) {
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <=m ; j++) {
+                dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+            }
+        }
+        return dy[m];
+    }
+
+    public static void main(String[] args) {
+        ExchangeCoins t = new ExchangeCoins();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        m = kb.nextInt();
+        dy = new int[m + 1];
+        System.out.println(t.solution(arr));
+    }
+
+}
