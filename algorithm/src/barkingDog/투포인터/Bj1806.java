2b57de4f546743f7b6f9d020fdfd3b962c162386
jsjune <avraskio@gmail.com>
2023-01-17 23:42:54 +0900

    :white_check_mark: update

---
+++ algorithm/src/barkingDog/투포인터/Bj1806.java
@@ -0,0 +1,18 @@
+package barkingDog.투포인터;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* 부분 합 */
+public class Bj1806 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+    }
+}
