e7b8cd585d011bb12538cb5b79af5788e9cbe58e
jsjune <avraskio@gmail.com>
2023-04-27 01:49:15 +0900

    :white_check_mark: update

---
+++ mysql-master-slave/src/main/java/com/example/mysqlmasterslave/MysqlMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlmasterslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlMasterSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlMasterSlaveApplication.class, args);
+    }
+
+}
---
+++ mysql-master-slave/src/test/java/com/example/mysqlmasterslave/MysqlMasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlmasterslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlMasterSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
