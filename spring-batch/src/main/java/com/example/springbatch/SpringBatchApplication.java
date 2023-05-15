f80ec4925924c633ccfbbf7403a7621b29aa763c
jsjune <avraskio@gmail.com>
2023-05-16 00:03:58 +0900

    :white_check_mark: update

--- spring-batch/src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ spring-batch/src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,10 +1,8 @@
-package io.spring.springbatch;
+package com.example.springbatch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchApplication {
 
--- spring-batch/src/test/java/com/example/springbatch/SpringBatchApplicationTests.java
+++ spring-batch/src/test/java/com/example/springbatch/SpringBatchApplicationTests.java
@@ -1,4 +1,4 @@
-package io.spring.springbatch;
+package com.example.springbatch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
